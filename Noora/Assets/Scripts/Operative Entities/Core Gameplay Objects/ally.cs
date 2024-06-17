fd8a60e7ebc96f24a0d6e42b3e30cb2fcfe1d11d
Mohammad Reza Ahmadi <mohrezahmd@gmail.com>
2024-06-17 10:40:33 +0100

    adding shader

--- Noora/Assets/Scripts/Operative Entities/Core Gameplay Objects/ally.cs
+++ Noora/Assets/Scripts/Operative Entities/Core Gameplay Objects/ally.cs
@@ -32,7 +32,7 @@ public class ally : OperativeEntity
 
     private void FixedUpdate()
     {
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, _minX, _maxX), transform.position.y);
+        // transform.position = new Vector2(Mathf.Clamp(transform.position.x, _minX, _maxX), transform.position.y);
 
         if (transform.position.y < _minY)
         {
